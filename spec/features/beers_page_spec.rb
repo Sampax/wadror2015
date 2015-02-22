e3e357900a452dad68c182199538b3606e5017ce
Sampo Laurila <sampo.laurila@gmail.com>
2015-02-23 00:24:31 +0200

    testit kuntoon

--- spec/features/beers_page_spec.rb
+++ spec/features/beers_page_spec.rb
@@ -1,33 +1,30 @@
 require 'rails_helper'
 
-describe "Beers page" do
-  let!(:user) { FactoryGirl.create :user }
-
+describe "Beers" do
   before :each do
+    FactoryGirl.create :user
+    FactoryGirl.create :style
     sign_in(username:"Pekka", password:"Foobar1")
   end
 
-  it "should not have any before been created" do
-    visit beers_path
-    expect(page).to have_content 'Listing beers'
-
-  end
-
-  it "doesn't allow beers with blank names" do
+  it "can be added if a valid name given" do
     visit new_beer_path
-    click_button('Create Beer')
-
-    expect(page).to have_content("Name can't be blank")
-    expect(Beer.count).to eq(0)
-  end
-
-  it "allows beers to be created" do
-    visit new_beer_path
-    fill_in('beer_name', with: 'Brixton')
+    fill_in('beer_name', with:'Karhu')
 
     expect{
       click_button('Create Beer')
     }.to change{Beer.count}.by(1)
   end
 
+  it "is not added if a invalid valid name given" do
+    visit new_beer_path
+
+    expect{
+      click_button('Create Beer')
+    }.to change{Beer.count}.by(0)
+
+    expect(current_path).to eq(beers_path)
+    expect(page).to have_content "New beer"
+    expect(page).to have_content "Name can't be blank"
+  end
 end
--- spec/features/ratings_spec.rb
+++ spec/features/ratings_spec.rb
@@ -1,59 +1,56 @@
 require 'rails_helper'
 
-describe "Rating" do
+describe "Ratings" do
   let!(:brewery) { FactoryGirl.create :brewery, name:"Koff" }
   let!(:beer1) { FactoryGirl.create :beer, name:"iso 3", brewery:brewery }
   let!(:beer2) { FactoryGirl.create :beer, name:"Karhu", brewery:brewery }
   let!(:user) { FactoryGirl.create :user }
-  let!(:user2) {FactoryGirl.create :user, username: "Simo" }
-  let!(:rating) { FactoryGirl.create :rating, user:user, beer:beer1}
-  let!(:rating2) { FactoryGirl.create :rating2, user:user2, beer:beer2}
 
   before :each do
     sign_in(username:"Pekka", password:"Foobar1")
   end
 
-  it "count and result is shown in rating index" do
-    visit ratings_path
-    expect(page).to have_content("Number of ratings: 2")
-
-    expect(page).to have_content("iso 3 10")
-    expect(page).to have_content("Karhu 20")
-
-  end
-
-  it "when given, is registered to the beer and user who is signed in" do
+  it "when one given, is registered to the beer and user who is signed in" do
     visit new_rating_path
     select('iso 3', from:'rating[beer_id]')
     fill_in('rating[score]', with:'15')
 
     expect{
       click_button "Create Rating"
-    }.to change{Rating.count}.from(2).to(3)
+    }.to change{Rating.count}.from(0).to(1)
 
-    expect(user.ratings.count).to eq(2)
-    expect(beer1.ratings.count).to eq(2)
-    expect(beer1.average_rating).to eq(12.5)
+    expect(user.ratings.count).to eq(1)
+    expect(beer1.ratings.count).to eq(1)
+    expect(beer1.average_rating).to eq(15.0)
   end
 
-  describe "given ratings" do
 
-    before :each do
-      visit user_path(user)
-    end
+  it "of one user are shown at user's page" do
+    my_rating1 = FactoryGirl.create(:rating, score:10, beer:beer1, user:user)
+    my_rating2 = FactoryGirl.create(:rating, score:30, beer:beer2, user:user)
+    other_rating = FactoryGirl.create(:rating, score:20, beer:beer1)
 
-    it "are shown on a user's page" do
-      expect(page).to have_content("iso 3 10")
-      expect(page).not_to have_content("Karhu 20")
-    end
+    visit user_path(user.id)
+    expect(page).to have_content "has made 2 ratings"
 
-    it "are properly removed from user's page and database" do
-      expect{
-        click_link "delete"
-      }.to change{Rating.count}.from(2).to(1)
-
-      expect(page).not_to have_content("iso 3 10")
-
-    end
+    expect(page).to have_content my_rating1.to_s
+    expect(page).to have_content my_rating2.to_s
+    expect(page).not_to have_content other_rating.to_s
   end
+
+  # it "can be removed by the user" do
+  #   my_rating1 = FactoryGirl.create(:rating, score:10, beer:beer1, user:user)
+  #   my_rating2 = FactoryGirl.create(:rating, score:30, beer:beer2, user:user)
+  #   other_rating = FactoryGirl.create(:rating, score:20, beer:beer1)
+
+  #   visit user_path(user.id)
+  #   deleted_rating = page.all('li')[1].text
+
+  #   expect{
+  #     page.all('a', text:'delete' )[1].click
+  #   }.to change{Rating.count}.from(3).to(2)
+
+  #   visit user_path(user.id)
+  #   expect(page).not_to have_content deleted_rating
+  # end
 end
